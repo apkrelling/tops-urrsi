299180ab273abaeba624b8162f2fb3362a30671e
Ana Krelling <ana@axiomdatascience.com>
2024-07-31 12:59:11 -0400

    add mean fun + test

--- math_operations.py
+++ math_operations.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def add(a, b):
     return a + b
 
@@ -14,3 +16,6 @@ print(
     " oh my gosh this line is sooooo darn long! But I mean, reeeealy long! but hey, the result is "
     + str(add(10, 5))
 )
+
+def mean(numbers):
+    return np.mean(numbers)
