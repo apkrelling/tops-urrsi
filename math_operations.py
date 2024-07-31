11d429132c034edae62cc59221806e5bd1c872c2
Ana Krelling <ana@axiomdatascience.com>
2024-07-31 12:10:41 -0400

    first commit

---
+++ math_operations.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+print(
+    " oh my gosh this line is sooooo darn long! But I mean, reeeealy long! but hey, the result is "
+    + str(add(10, 5))
+)
